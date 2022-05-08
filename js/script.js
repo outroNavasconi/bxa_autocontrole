b46f7fce7e6fc911c4b1cc622ff2a21bce82b17f
outronavasconi <seguindoovelhas@gmail.com>
2022-05-08 12:14:12 -0300

    Exlcusões de arquivos e implementação das animações dos textos

--- js/script.js
+++ js/script.js
@@ -1,7 +1,27 @@
 const body = document.querySelector("body")
 const head = document.getElementById("head")
+const personControlled = document.getElementById("personControlled")
+const personNotControlled = document.getElementById("personNotControlled")
 
 head.onclick = () => {
+	letHeadFall()
+	changeText()
+}
+
+function letHeadFall() {
 	const value = body.dataset.headLost
-	body.dataset.headLost = ["unset", "false"].includes(value) ? "true" : "false"
+	if (value === "mantaing")
+		body.dataset.headLost = "falling"
+	else
+		rising()
+}
+
+function rising() {
+	body.dataset.headLost = "rising"
+	setTimeout(() => body.dataset.headLost = "mantaing", 1700)
+}
+
+function changeText() {
+	personControlled.classList.toggle("youAreHere")
+	personNotControlled.classList.toggle("youAreHere")
 }
