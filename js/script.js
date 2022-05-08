984e1ab7fbe77dab7d87d9bb940e59c9fd98bed4
outronavasconi <seguindoovelhas@gmail.com>
2022-05-08 08:48:06 -0300

    Criação dos arquivos da nova versão

--- js/script.js
+++ js/script.js
@@ -1,32 +1,7 @@
-const cabeca = document.getElementById("cabeca")
-const fundoCabeca = document.getElementById("fundoCabeca")
-const issoEhBom = document.getElementById("issoEhBom")
-const issoNemTanto = document.getElementById("issoNemTanto")
+const body = document.querySelector("body")
+const head = document.getElementById("head")
 
-cabeca.addEventListener("click", mudarEstadoMental) 
-
-function mudarEstadoMental() {
-  ativarAnimacoes()
-  perdendoACabeca()
-  precisandoConversar()
+head.onclick = () => {
+	const value = body.dataset.headLost
+	body.dataset.headLost = ["unset", "false"].includes(value) ? "true" : "false"
 }
-
-function ativarAnimacoes() {
-  const animacoes = document.querySelector("[data-estado-animacoes]")
-  if (animacoes.dataset.estadoAnimacoes === "false")
-    animacoes.dataset.estadoAnimacoes = "true"
-}
-
-function perdendoACabeca() {
-  fundoCabeca.classList.toggle("perdeuControle")
-  if (fundoCabeca.classList.contains("perdeuControle"))
-    cabeca.style.right = "56px"
-  else 
-    cabeca.style.right = "2px"
-}
-
-function precisandoConversar() {
-  issoEhBom.classList.toggle("seuEstadoEhEsse")
-  issoNemTanto.classList.toggle("seuEstadoEhEsse")
-}
-
---
+++ js/script_old.js
@@ -0,0 +1,37 @@
+// const cabeca = document.getElementById("cabeca")
+// const wrapper = document.getElementById("wrapper")
+// // const fundoCabeca = document.getElementById("fundoCabeca")
+// const issoEhBom = document.getElementById("issoEhBom")
+// const issoNemTanto = document.getElementById("issoNemTanto")
+
+// cabeca.addEventListener("click", mudarEstadoMental) 
+
+// cabeca.onclick = () => cabeca.classList.toggle("teste")
+// wrapper.onclick = () => wrapper.classList.toggle("teste")
+
+
+// function mudarEstadoMental() {
+//   ativarAnimacoes()
+//   perdendoACabeca()
+//   // precisandoConversar()
+// }
+
+// function ativarAnimacoes() {
+//   const animacoes = document.querySelector("[data-estado-animacoes]")
+//   if (animacoes.dataset.estadoAnimacoes === "false")
+//     animacoes.dataset.estadoAnimacoes = "true"
+// }
+
+// function perdendoACabeca() {
+//   fundoCabeca.classList.toggle("perdeuControle")
+//   if (fundoCabeca.classList.contains("perdeuControle"))
+//     cabeca.style.right = "56px"
+//   else 
+//     cabeca.style.right = "2px"
+// }
+
+// function precisandoConversar() {
+//   issoEhBom.classList.toggle("seuEstadoEhEsse")
+//   issoNemTanto.classList.toggle("seuEstadoEhEsse")
+// }
+
